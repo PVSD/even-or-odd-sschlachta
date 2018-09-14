d5f2de45deeb73b88062d64a4634512040ba7bcb
sschlachta <42655286+sschlachta@users.noreply.github.com>
2018-09-13 20:36:38 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        import java.io.*;
+        import java.util.*;
+        public class Tester{
+
+            public static void main(String[] args){
+
+                Scanner enterInt = new Scanner(System.in);
+                System.out.println("Enter an integer:");
+                int theNumber = enterInt.nextInt();
+
+                if (theNumber % 2 == 0) {
+
+                    System.out.println("The integer " + theNumber + "is even.");
+
+                } else {
+
+                    System.out.println("The integer " + theNumber + "is odd.");
+
+                }
+
+                }
+        }
     }
+
+
 }
